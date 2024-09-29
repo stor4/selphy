3fb93cf1e8a39622136b69b1219239f244dea923
stor4 <storchak850@gmail.com>
2024-09-29 13:05:07 +0300

    style fix

--- src/js/_components.js
+++ src/js/_components.js
@@ -3,7 +3,7 @@ console.log('components');
 // import './components/jquery-price-slider.js'
 import './components/plugins.js'
 import './components/bootstrap.min.js'
-// import './components/dbclick.min.js'
+import './components/dbclick.min.js'
 import './components/swipers.js'
 // import './components/jquery.meanmenu.min.js'
 import './components/scrollUp.min.js'
